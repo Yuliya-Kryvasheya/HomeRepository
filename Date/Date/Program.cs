20feb00c129211964b6a5a7bd4f25ee93ec364fc
Yuliya <Redangel1982@yandex.ru>
2021-04-06 23:28:21 +0300

    DayWeek branch added

---
+++ Date/Date/Program.cs
@@ -0,0 +1,298 @@
+﻿using System;
+
+namespace Date
+    
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.Write("Enter the day of the week: ");
+            string DayWeek = Console.ReadLine();
+            if (DayWeek.Equals("Monday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("Mon"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("mon"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("MON"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("monday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("MO"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("mo"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("Mo"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Monday - 1, 5 days till the weekend");
+            }
+            else if (DayWeek.Equals("Tuesday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("Tue"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("tue"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("TUE"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("tuesday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("TU"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("tu"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("Tu"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tuesday - 2, 4 days till the weekend");
+            }
+            else if (DayWeek.Equals("Wednesday"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("Wed"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("wed"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("WED"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("wednesday"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("WE"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("we"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("We"))
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wednesday - 3, 3 days till the weekend");
+            }
+            else if (DayWeek.Equals("Thursday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("Thu"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("thu"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("THU"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("thursday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("TH"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("th"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("Th"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Thursday - 4, 2 days till the weekend");
+            }
+            else if (DayWeek.Equals("Friday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("Fri"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("fri"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("FRI"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("friday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("FR"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("fr"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("Fr"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Friday - 5, 1 days till the weekend");
+            }
+            else if (DayWeek.Equals("Saturday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("Sat"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("sat"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("SAT"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("saturday"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("SA"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("sa"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("Sa"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Saturday - 6, weekend");
+            }
+            else if (DayWeek.Equals("Sunday"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (DayWeek.Equals("Sun"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (DayWeek.Equals("sun"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (DayWeek.Equals("SUN"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (DayWeek.Equals("sunday"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (DayWeek.Equals("SU"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (DayWeek.Equals("su"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (DayWeek.Equals("Su"))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Sunday - 7, weekend");
+            }
+            else if (!DayWeek.Equals("sunday", StringComparison.Ordinal))
+            {
+                Console.WriteLine("There is no such day");
+            }
+        }
+    }
+}
