721fcd2425fc172999441f43d89116bb10866202
Yuliya <Redangel1982@yandex.ru>
2021-04-11 13:16:14 +0300

    Task1 and Task 2 commited

---
+++ Attow-1/Attow-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Exercise1
+{
+    public static void Main()
+    {
+        int[] a = new int[10];
+        int i;
+
+        Console.Write("Input 10 elements in the array :\n");
+        for (i = 0; i < 10; i++)
+        {
+            Console.Write("element - {0} : ", i);
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("\nElements in array are: ");
+        for (i = 0; i < 10; i++)
+        {
+            Console.Write("{0}  ", a[i]);
+        }
+        Console.Write("\n");
+    }
+}
