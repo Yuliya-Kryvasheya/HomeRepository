d4d5a6216d5dee044094c543b93c605268ae377b
Yuliya <Redangel1982@yandex.ru>
2021-04-11 15:40:17 +0300

    String Task4 implemented

---
+++ String-4/String-4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int i = 0;
+            Console.Write("Input the string: ");
+            s = Console.ReadLine();
+            i = s.Length - 1;
+            Console.Write("The characters of the string in reverse are: \n");
+            _ = s.Split(' ');
+
+            foreach (var c1 in s)
+            
+            while (i >= 0)
+            {
+                Console.Write("{0} ", s[i]);
+                i--;
+            }
+            Console.Write("\n\n");
+        }
+        
+    }
+}
