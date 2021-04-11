64ceb480f4d6c223f32b13fba3ed9fd1df0eedef
Yuliya <Redangel1982@yandex.ru>
2021-04-11 14:46:02 +0300

    String Task2 implemented

---
+++ String-3/String-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Input the string: ");
+            s = Console.ReadLine();
+
+            Console.Write("The characters of the string are:  ");
+            _ = s.Split(' ');
+
+            foreach (var c1 in s)
+            {
+                Console.Write($" {c1} ");
+            }
+            
+        }
+    }
+}
