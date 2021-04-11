56f31ba3483ec5b47ad5d567b0ff878f754e079f
Yuliya <Redangel1982@yandex.ru>
2021-04-11 13:26:46 +0300

    Task 4 implemented

---
+++ Arrow-4/Arrow-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Task4
+{
+    public static void Main()
+    {
+        int[] a = new int[50];
+        int n;
+        Console.Write("Input the number of elements to store in the array :");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input 10 elements in the array :\n");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("element - {0} : ", i);
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("\nThe values store into the array are: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("{0}  ", a[i]);
+        }
+        Console.Write("\n");
+
+        Console.Write("\nThe elements copied into the second array are: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("{0}  ", a[i]);
+        }
+        Console.Write("\n");
+    }
+}
