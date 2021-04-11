15f9d88441a7f8fcce1536f722e5fc2ce2d17721
Yuliya <Redangel1982@yandex.ru>
2021-04-11 15:49:33 +0300

    String Task5 implemented

---
+++ String-5/String-5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Input the string: ");
+            s = Console.ReadLine();
+
+            int s1 = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            Console.Write("Total number of words in the string is: {0}\n", s1);
+        }
+    }
+}
