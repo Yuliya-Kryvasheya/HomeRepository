06f2f21cd3f578c6b69e11c1d0801e8271a5858d
Yuliya <Redangel1982@yandex.ru>
2021-04-11 14:12:37 +0300

    String Task2 implemented

---
+++ String-2/String-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Input the string: ");
+            s = Console.ReadLine();
+            int s1 = s.Length;
+            Console.Write("Length of the string is: {0}\n", s1);
+        }
+    }
+}
