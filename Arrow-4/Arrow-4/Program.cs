ffce18799ee773ac47725b5bb42372aae04234cc
Yuliya <Redangel1982@yandex.ru>
2021-04-12 13:02:59 +0300

    Arrow Task-4 fix

--- Arrow-4/Arrow-4/Program.cs
+++ Arrow-4/Arrow-4/Program.cs
@@ -1,33 +1,38 @@
 ﻿using System;
-
 public class Task4
 {
     public static void Main()
     {
         int[] a = new int[50];
-        int n;
-        Console.Write("Input the number of elements to store in the array :");
+        int[] b = new int[50];
+        int i, n;
+
+        Console.Write("Input the number of elements to be stored in the array :");
         n = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Input 10 elements in the array :\n");
-        for (int i = 0; i < n; i++)
+        Console.Write("Input {0} elements in the array :\n", n);
+        for (i = 0; i < n; i++)
         {
             Console.Write("element - {0} : ", i);
             a[i] = Convert.ToInt32(Console.ReadLine());
         }
+        
+        for (i = 0; i < n; i++)
+        {
+            b[i] = a[i];
+        }
 
-        Console.Write("\nThe values store into the array are: ");
-        for (int i = 0; i < n; i++)
+        Console.Write("\nThe elements stored in the first array are :\n");
+        for (i = 0; i < n; i++)
         {
             Console.Write("{0}  ", a[i]);
         }
-        Console.Write("\n");
 
-        Console.Write("\nThe elements copied into the second array are: ");
-        for (int i = 0; i < n; i++)
+        Console.Write("\n\nThe elements copied into the second array are :\n");
+        for (i = 0; i < n; i++)
         {
-            Console.Write("{0}  ", a[i]);
+            Console.Write("{0}  ", b[i]);
         }
-        Console.Write("\n");
+        Console.Write("\n\n");
     }
 }
