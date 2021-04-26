f5b4b301ca43441bc770dc4fcafa6ee6b40bf3f8
Yuliya <Redangel1982@yandex.ru>
2021-04-26 10:09:56 +0300

    Bank applicatioin

---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using BankLibrary;
+
+namespace Bank
+{
+    public class Program
+    {
+        private static readonly Bank<Account> s_bank1 = new(Write);
+
+        public static void Main(string[] args)
+        {
+            var alive = true;
+            while (alive)
+            {
+                var color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("1. Open Account \t 2. Withdraw sum \t 3. Add sum");
+                Console.WriteLine("4. Close Account \t 5. Skip day \t 6. Exit program");
+                Console.WriteLine("Enter the item number:");
+                Console.ForegroundColor = color;
+                try
+                {
+                    var choice = Convert.ToInt32(Console.ReadLine(), (IFormatProvider)default);
+
+                    Console.Clear();
+
+                    switch (choice)
+                    {
+                        case 1:
+                            OpenAccount();
+                            break;
+                        case 2:
+                            Withdraw();
+                            break;
+                        case 3:
+                            Put();
+                            break;
+                        case 4:
+                            CloseAccount();
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            alive = false;
+                            continue;
+                    }
+                    CalculatePercentage();
+                }
+                catch (Exception ex)
+                {
+                    color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = color;
+                }
+                DisplayAccounts();
+            }
+        }
+
+        private static void DisplayAccounts()
+        {
+            s_bank1.DisplayAccounts();
+        }
+
+        public static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private static void CalculatePercentage()
+        {
+            s_bank1.IncrementDays();
+        }
+
+        private static void OpenAccount()
+        {
+            Console.WriteLine("Specify the sum to create an account: ");
+            var sum = Convert.ToDecimal(Console.ReadLine(), default);
+
+            Console.WriteLine("Select an account type: \n 1. On-Demand \n 2. Deposit");
+            var type = Enum.Parse<AccountType>(Console.ReadLine()!);
+
+            s_bank1.OpenAccount(new OpenAccountParameters
+            {
+                Amount = sum,
+                Type = type,
+                AccountNotify = Write
+            });
+        }
+
+        private static void Withdraw()
+        {
+            Console.WriteLine("Specify the sum to withdraw from the account: ");
+            var sum = Convert.ToDecimal(Console.ReadLine(), default);
+
+            Console.WriteLine("Enter account id: ");
+            var id = Convert.ToInt32(Console.ReadLine(), (IFormatProvider)default);
+
+            s_bank1.Withdraw(id, sum);
+        }
+
+        private static void Put()
+        {
+            Console.WriteLine("Specify the sum to put on the account: ");
+            var sum = Convert.ToDecimal(Console.ReadLine(), default);
+
+            Console.WriteLine("Enter account id: ");
+            var id = Convert.ToInt32(Console.ReadLine(), (IFormatProvider)default);
+
+            s_bank1.Put(id, sum);
+        }
+
+        private static void CloseAccount()
+        {
+            Console.WriteLine("Enter the account id to close: ");
+            var id = Convert.ToInt32(Console.ReadLine(), (IFormatProvider)default);
+
+            s_bank1.CloseAccount(id);
+        }
+
+    }
+}
---
+++ Bank/BankLibrary/Bank.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankLibrary
+{
+    public class Bank<T> where T : Account
+    {
+        public Action<string> _writeOutput;
+
+        private readonly List<T> _accounts = new();
+
+        public Bank(Action<string> writeOutput)
+        {
+            _writeOutput = writeOutput;
+        }
+        public void OpenAccount(OpenAccountParameters parameters)
+        {
+            AssertValidType(parameters.Type);
+            CreateAccount(parameters.AccountNotify, () => parameters.Type == AccountType.Deposit
+                ? new DepositAccount(parameters.Amount) as T
+                : new OnDemandAccount(parameters.Amount) as T);
+        }
+
+        private static void AssertValidType(AccountType type)
+        {
+            var itemType = typeof(T);
+            if (itemType != typeof(Account) && ((type == AccountType.Deposit && itemType != typeof(DepositAccount))
+                                           || (type == AccountType.OnDemand && itemType != typeof(OnDemandAccount))))
+            {
+                throw new InvalidOperationException("Ivalid account type.");
+            }
+
+        }
+
+        private void CreateAccount(AccountNotification accountNotify, Func<T> creator)
+        {
+            var account = creator();
+            account.Notify += accountNotify;
+            account.Open();
+
+            _accounts.Add(account);
+        }
+        public void Withdraw(int id, decimal amount)
+        {
+            AssertValidId(id--);
+            _accounts[id].Withdraw(amount);
+        }
+
+        private void AssertValidId(int id)
+        {
+            if (id < 1 || id > _accounts.Count)
+            {
+                throw new InvalidOperationException("Not valid Id");
+            }
+        }
+
+        public void Put(int id, decimal amount)
+        {
+            AssertValidId(id--);
+            _accounts[id].Put(amount);
+        }
+
+        public void CloseAccount(int id)
+        {
+            AssertValidId(id--);
+            _accounts[id].Close();
+        }
+
+        public void DisplayAccounts()
+        {
+            foreach (var item in _accounts)
+            {
+                _writeOutput?.Invoke($"{item}");
+            }
+        }
+        public void IncrementDays()
+        {
+            foreach (var item in _accounts)
+            {
+                item.IncrementDays();
+            }
+        }
+    }
+}
